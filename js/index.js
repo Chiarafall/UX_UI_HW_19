eac32c1589096346525b0e4579b16498b8dd4e8d
Chiara <fall.chiara@gmail.com>
2023-08-01 23:15:38 -0400

    Added some JS
    - Added "ScrollReveal" to the projects
    - Added a bouncing Arrow but still not working

--- js/index.js
+++ js/index.js
@@ -1,9 +1,22 @@
 console.log ("index.js file is loaded correctly!");
 
-ScrollReveal().reveal(".projects");
+function fade() {
+    $('.preloader').fadeOut("slow");
+    }
+    setTimeout(fade, 3000);
+
+// Arrow bouncing on hover (not working):
+function bounce(){
+  $(".center-arrow").bounce("slow");
+  setTimeout(isFinite);
+  
+}
+
+// revealing the card as scrolling ("Coming Soon" not working): from:https://scrollrevealjs.org/
+ScrollReveal().reveal('#rogen')
+ScrollReveal().reveal('#cbc', { delay: 500 })
+ScrollReveal().reveal('#healink', { delay: 800})
+ScrollReveal().reveal('#ComingSoon', { delay: 1000})
+
 ScrollReveal({ reset: true });
-
-
-
-
 
